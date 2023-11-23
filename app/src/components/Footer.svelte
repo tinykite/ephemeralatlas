d7ab023aab2ae710623f36bec9820578cb58ae85
tinykite <dakota@tinykitelab.com>
2023-11-23 09:27:42 -0600

    feat: Transfer components from tinykitelab.com

---
+++ app/src/components/Footer.svelte
@@ -0,0 +1,22 @@
+<footer>
+	View this website's source code on
+	<a href="https://github.com/tinykite/tinykitelab.com">GitHub</a> or
+	<a class="email" href="mailto:dakota@tinykitelab.com">Say Hello</a> | Find
+	me on
+	<a href="https://www.instagram.com/tinykitelab/">Instagram</a>
+</footer>
+
+<style>
+	footer {
+  	text-align: center;
+  	margin: 3rem;
+  	padding-bottom: 3rem;
+  	font-family: monospace;
+	}
+
+	@media (min-width: 43.75rem) {
+		footer {
+    	margin: 6.25rem 0 0 0;
+		}
+  }
+</style>
