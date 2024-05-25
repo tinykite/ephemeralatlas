79182f2eafb154979777250a8f832eebfedfc077
tinykite <dakota@tinykitelab.com>
2024-05-25 15:53:37 -0500

    Add nav items

---
+++ app/src/lib/utils/consts.ts
@@ -0,0 +1,14 @@
+export const navItems = [
+    {
+        name: "Archive", 
+        url: "/archive"
+    }, 
+    {
+        name: "About", 
+        url: "/about"
+    },
+    {
+        name: "Donate", 
+        url: "/donate"
+    }
+]
