1c3312d71fdb39e4f31e32c22ad3603e0ba22e52
tinykite <dakota@tinykitelab.com>
2024-05-25 16:01:07 -0500

    Add Nav

--- app/src/components/Nav.svelte
+++ app/src/components/Nav.svelte
@@ -1,18 +1,59 @@
+<script>
+	import Logo from '$components/Logo.svelte';
+	import { navItems } from '$lib/utils/consts';
+</script>
+
 <header class="header">
+	<p class="header__tagline">Creative Explorations of Nature</p>
+	<Logo />
 	<nav class="nav">
-		<a href="/">
-			<svg aria-hidden="true" class="logo" width="75" height="75" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path fill-rule="evenodd" clip-rule="evenodd" d="M512 0H0V512H512V0ZM198.415 218.68H129.121L256 89.4324L382.76 218.68H312.132C357.357 261.407 414.919 315.952 424.342 325.657C417.234 327.434 371.743 338.214 322.223 326.486C290.88 319.078 270.197 278.163 260.847 259.667C258.535 255.093 256.916 251.89 256 250.904C253.933 252.557 251.277 258.017 247.742 265.286L247.742 265.287C238.105 285.099 221.932 318.35 193.331 324.591C143.574 335.253 99.2674 327.789 87.7761 325.657H87.6576C136.737 277.509 172.668 243.009 198.415 218.68ZM424.342 349.706C390.816 382.64 328.028 404.793 256 404.793C183.972 404.793 121.184 382.64 87.6576 349.706H424.342Z" fill="#231F20"/>
-				</svg>
-			<span class="u-visually-hidden">Minimal Botany</span>
-		</a>
+		<ul class="navList">
+			{#each navItems as navItem}
+				<li class="navList__item">
+					<a href={navItem.url}>
+						{navItem.name}
+					</a>
+				</li>
+			{/each}
+		</ul>
 	</nav>
 </header>
 
 <style>
 	.header {
+		display: grid;
+		grid-template-columns: 1fr 4.6875rem 1fr;
+		justify-content: space-between;
+		align-items: center;
+		margin-block-start: 3.125rem;
+		margin-inline: 5rem;
+	}
+	.header__tagline,
+	.nav {
+		font-family: 'Nimbus Mono PS', 'Courier New', monospace;
+	}
+
+	.nav {
+		justify-self: end;
+	}
+
+	.header__tagline,
+	.navList {
+		font-size: 1rem;
+	}
+
+	.navList {
 		display: flex;
-		justify-content: center;
-		margin-top: 5rem;
+		list-style: none;
+		margin: 0;
+		font-family: 'Nimbus Mono PS', 'Courier New', monospace;
+	}
+
+	.navList__item {
+		text-decoration: none;
+	}
+
+	.navList__item:not(:last-child) {
+		margin-inline-end: 1.8125rem;
 	}
 </style>
