939b0b827b8231d128f656de5b5bf98e2da42e82
tinykite <dakota@tinykitelab.com>
2024-06-02 18:38:00 -0500

    Add placeholder pages

---
+++ app/src/routes/about/+page.svelte
@@ -0,0 +1,6 @@
+<main class="wrapper">
+	<article class="article">
+		<h1 class="article__title">About</h1>
+		<p>Coming Soon</p>
+	</article>
+</main>
---
+++ app/src/routes/donate/+page.svelte
@@ -0,0 +1,6 @@
+<main class="wrapper">
+	<article class="article">
+		<h1 class="article__title">Donate</h1>
+		<p>Coming Soon</p>
+	</article>
+</main>
