3f73a798336cc0a9efa1f82a4756d302b2398b40
tinykite <dakota@tinykitelab.com>
2024-06-02 15:09:39 -0500

    Add tokens css to website and clean up global css

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	// Importing this in the layout, instead of app.html, to enable HMR
+	import '../styles/tokens.css';
 	import '../styles/global.css';
 	import Nav from '../components/Nav.svelte';
 	import Footer from '$components/Footer.svelte';
