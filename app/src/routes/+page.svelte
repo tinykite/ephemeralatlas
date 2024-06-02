39350ce9d42aab1b6838bc8f18a19b7e56f5f9ec
tinykite <dakota@tinykitelab.com>
2024-06-02 16:09:54 -0500

    Add basic responsive mobile styles to homepage

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -39,35 +39,36 @@
 		</p>
 	</aside>
 
-	<h2>Archives</h2>
-	<ul class="articleList">
-		{#each archive as post, i}
-			<li class="articleList__item">
-				{#if post.mainIllustration}
-					<Illustration name={post.mainIllustration} />
-				{:else if post.mainImage}
-					<img
-						class="articleList__image"
-						loading="lazy"
-						src={urlFor(post.mainImage).width(600).height(400).quality(100).auto('format').url()}
-						alt={post.mainImage.alt}
-						width="600"
-						height="400"
-					/>
-				{/if}
+	<div class="archives">
+		<h2 class="archives__heading">Archives</h2>
+		<ul class="articleList">
+			{#each archive as post, i}
+				<li class="articleList__item">
+					{#if post.mainIllustration}
+						<Illustration name={post.mainIllustration} />
+					{:else if post.mainImage}
+						<img
+							class="articleList__image"
+							loading="lazy"
+							src={urlFor(post.mainImage).width(600).height(400).quality(100).auto('format').url()}
+							alt={post.mainImage.alt}
+							width="600"
+							height="400"
+						/>
+					{/if}
 
-				<div class="articleList__text">
-					<p class="articleList__date">{formatDate(post.publishedAt)}</p>
-
-					<a class="articleList__link" href={`/${post.slug.current}`}>
-						{post.title}
-					</a>
-				</div>
-			</li>
-		{/each}
-	</ul>
+					<div class="articleList__text">
+						<a class="articleList__link" href={`/${post.slug.current}`}>
+							{post.title}
+						</a>
+					</div>
+				</li>
+			{/each}
+		</ul>
+	</div>
 </main>
 
+<!-- In the process of being converted to design tokens -->
 <style>
 	.homepageWrapper {
 		max-width: 70rem;
@@ -76,7 +77,7 @@
 
 	.latestArticle {
 		text-align: center;
-		margin: 3rem auto;
+		margin: var(--spacing-36) auto;
 	}
 
 	.latestArticle__contentWrapper {
@@ -86,7 +87,7 @@
 
 	.latestArticle__link {
 		text-decoration: none;
-		font-family: var(--f-headings);
+		font-family: var(--font-stack-headings);
 		font-weight: 600;
 
 		&:hover {
@@ -99,47 +100,75 @@
 	}
 
 	.latestArticle__title {
-		margin-block-start: 0.5rem;
+		margin-block-start: var(--spacing-8);
+		margin-inline: var(--spacing-8);
+	}
+
+	.archives {
+		display: grid;
+		justify-items: center;
+		margin-block-start: var(--spacing-24);
+
+		@media (min-width: 45rem) {
+			justify-items: unset;
+			margin-block-start: var(--spacing-64);
+		}
+	}
+
+	/* Should become a utility class */
+	.archives__heading {
+		margin: 0;
 	}
 
 	.articleList {
 		display: grid;
-		grid-auto-flow: column;
-		column-gap: 3.75rem;
 		list-style: none;
 		padding: 0;
-		margin: 0 auto;
-		max-width: 70rem;
 		margin-block-start: 1.5rem;
+		margin-inline: auto;
+
+		@media (min-width: 45rem) {
+			grid-auto-flow: column;
+			column-gap: var(--spacing-60);
+			max-width: 70rem;
+		}
 	}
 
 	.articleList__item {
-		max-width: 30rem;
 		display: flex;
 		flex-direction: column;
-		padding-block-end: 1.5rem;
+		text-align: center;
+
+		@media (min-width: 45rem) {
+			text-align: unset;
+		}
+	}
+
+	.articleList__item:not(:first-of-type) {
+		margin-block-start: var(--spacing-32);
+
+		@media (min-width: 45rem) {
+			margin-block-start: unset;
+		}
 	}
 
 	.articleList__link {
 		display: block;
-		margin-block-start: 0.25rem;
+		margin-block-start: var(--spacing-16);
 		text-decoration: none;
-		font-size: 1.25rem;
-		font-family: var(--f-headings);
+		font-family: var(--font-stack-headings);
 		font-weight: 400;
 		font-style: normal;
 		font-stretch: normal;
 		line-height: 1.125;
+
+		@media (min-width: 45rem) {
+			font-size: var(--font-size-300);
+		}
 	}
 
 	.articleList__link:hover {
 		text-decoration: underline;
-	}
-
-	.articleList__date {
-		margin-block-start: 0.5rem;
-		font-size: 1rem;
-		font-family: 'Courier New', Courier, monospace;
 	}
 
 	.articleList__image {
@@ -149,36 +178,43 @@
 	.intro {
 		background-color: #ffb82e;
 		position: relative;
-		margin-block: 5rem;
 
-		&::before {
-			content: '';
-			display: block;
-			position: absolute;
-			width: 81px;
-			height: 68px;
-			left: 2rem;
-			top: 2rem;
-			background-image: url(/illustrations/flower.svg);
-		}
+		@media (min-width: 45rem) {
+			margin-block-start: var(--spacing-64);
 
-		&::after {
-			content: '';
-			display: block;
-			position: absolute;
-			width: 81px;
-			height: 68px;
-			right: 2rem;
-			bottom: 2rem;
-			background-image: url(/illustrations/flower.svg);
+			&::before {
+				content: '';
+				display: block;
+				position: absolute;
+				width: 81px;
+				height: 68px;
+				left: 2rem;
+				top: 2rem;
+				background-image: url(/illustrations/flower.svg);
+			}
+
+			&::after {
+				content: '';
+				display: block;
+				position: absolute;
+				width: 81px;
+				height: 68px;
+				right: 2rem;
+				bottom: 2rem;
+				background-image: url(/illustrations/flower.svg);
+			}
 		}
 	}
 
 	.intro__content {
-		font-family: var(--f-subheadings);
-		font-size: 1.5rem;
+		padding: var(--spacing-64);
+		font-family: var(--font-stack-subheadings);
 		text-align: center;
-		padding-block: 8rem;
-		padding-inline: 9.5rem;
+
+		@media (min-width: 45rem) {
+			padding-block: 8rem;
+			padding-inline: 9.5rem;
+			font-size: var(--font-size-500);
+		}
 	}
 </style>
