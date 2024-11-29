b913798c6e2e598bcbae08b352bbcb655f11d6bb
tinykite <dakota@tinykitelab.com>
2024-11-29 10:19:01 -0600

    Remove convoluted navigation logic

--- app/src/components/Nav.svelte
+++ app/src/components/Nav.svelte
@@ -1,30 +1,9 @@
 <script lang="ts">
-	import { onMount, onDestroy } from 'svelte';
 	import Logo from '$components/Logo.svelte';
 	import { navItems } from '$lib/utils/consts';
-
-	let mediaQuery: MediaQueryList;
-	let isMinDesktop: boolean | undefined = $state(true);
-
-	const setMatches = () => {
-		isMinDesktop = mediaQuery.matches;
-	};
-
-	onMount(() => {
-		mediaQuery = window.matchMedia('(min-width: 800px)');
-		setMatches();
-		mediaQuery.addEventListener('change', () => setMatches());
-	});
-
-	onDestroy(() => {
-		mediaQuery && mediaQuery.removeEventListener('change', () => setMatches());
-	});
 </script>
 
 <header class="header">
-	<p class="header__tagline" hidden={isMinDesktop ? undefined : true}>
-		Creative Explorations of Nature
-	</p>
 	<Logo />
 	<nav class="nav">
 		<ul class="navList">
@@ -46,15 +25,7 @@
 		justify-content: space-between;
 		align-items: center;
 		margin: 2rem;
-
-		@media (min-width: 50rem) {
-			grid-template-columns: 1fr 4.6875rem 1fr;
-			margin-inline: 5rem;
-			margin-block-start: 3.125rem;
-		}
 	}
-
-	.header__tagline,
 	.navList {
 		font-size: 1rem;
 		font-family: 'Nimbus Mono PS', 'Courier New', monospace;
