642c0f6e22fe465a7144240e88609eac491cb3b9
tinykite <dakota@tinykitelab.com>
2024-11-29 10:46:17 -0600

    Remove unnecessary types

--- app/src/routes/[slug]/+page.svelte
+++ app/src/routes/[slug]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import Article from '$components/Article.svelte';
-	interface Props { [key: string]: any }
+	interface Props {
+		[key: string]: any;
+	}
 
 	let { data, ...rest }: Props = $props();
 	rest;
