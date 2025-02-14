92e160eb8d39809f100f7ce9d5e4c020ba92d8ff
tinykite <dakota@tinykitelab.com>
2025-02-14 14:31:32 -0600

    Add supabase

--- app/src/lib/supbaseClientPrivate.ts
+++ app/src/lib/supbaseClientPrivate.ts
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 import { PRIVATE_SUPABASE_URL } from '$env/static/private';
 import { PRIVATE_SUPABASE_KEY } from '$env/static/private';
 
