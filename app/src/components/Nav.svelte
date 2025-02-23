acc0af74c972720909f9c617377137f76b56b206
tinykite <dakota@tinykitelab.com>
2025-02-23 13:05:22 -0600

    Add mobile nav

--- app/src/components/Nav.svelte
+++ app/src/components/Nav.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
 	import Logo from '$components/Logo.svelte';
 	import { navItems } from '$lib/utils/consts';
+
+	let menuToggled = $state(false);
 </script>
 
 <header class="header">
 	<Logo />
 	<nav class="nav">
+		<button class="button" aria-expanded={menuToggled} onclick={() => (menuToggled = !menuToggled)}>
+			<span class="button__content">
+				<span class="u-visually-hidden">Toggle Main</span>
+				Menu
+			</span>
+		</button>
 		<ul class="navList">
 			{#each navItems as navItem}
 				<li class="navList__item">
@@ -22,26 +30,103 @@
 	.header {
 		display: grid;
 		grid-template-columns: 4.6875rem 1fr;
-		justify-content: space-between;
+		row-gap: 1rem;
 		align-items: center;
+		position: relative;
 	}
-	.navList {
-		display: flex;
-		justify-content: flex-end;
-		list-style: none;
-		margin: 0;
-		font-family: var(--font-stack-subheadings);
-		font-size: 1rem;
-	}
-	.navList__link {
-		text-decoration: none;
 
-		&:hover {
-			border-bottom: 1px solid;
+	.nav {
+		grid-row: 2;
+		grid-column: 1 / -1;
+
+		@media (min-width: 50rem) {
+			grid-row: 1;
+			grid-column: 2 / -1;
 		}
 	}
 
-	.navList__item:not(:last-child) {
-		margin-inline-end: 1.8125rem;
+	.nav:has(button[aria-expanded='true']) .navList {
+		display: block;
+
+		@media (min-width: 50rem) {
+			display: flex;
+		}
+	}
+
+	.navList {
+		display: none;
+		padding: 0;
+		list-style: none;
+		font-family: var(--font-stack-subheadings);
+		font-size: 1rem;
+		text-align: center;
+
+		@media (min-width: 50rem) {
+			display: flex;
+			justify-self: end;
+			text-align: left;
+		}
+	}
+	.navList__link {
+		display: block;
+		text-decoration: none;
+		padding-block: 1rem;
+		margin: 0;
+
+		&:hover {
+			background: #000;
+			color: white;
+		}
+
+		@media (max-width: 50rem) {
+			&:before {
+				content: '';
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				border-bottom: 1px solid #000;
+			}
+		}
+		@media (min-width: 50rem) {
+			&:hover {
+				text-decoration: underline;
+				background: none;
+				color: unset;
+			}
+		}
+	}
+
+	.navList__item {
+		width: 100%;
+		position: relative;
+		margin: 0;
+
+		@media (min-width: 50rem) {
+			&:not(:last-child) {
+				margin-inline-end: 1.8125rem;
+			}
+		}
+	}
+
+	.button {
+		position: absolute;
+		top: calc(4.375rem / 2); /* 4.375rem is 70px, the height of the logo */
+		transform: translateY(-50%);
+		right: 0;
+		width: max-content;
+		border-radius: 0.25rem;
+		border: transparent;
+		background: #222222;
+		color: #f2f2f2;
+		font-family: var(--font-stack-subheadings);
+		font-size: 0.85rem;
+		padding: 0.25rem 0.5rem;
+		cursor: pointer;
+
+		@media (min-width: 50rem) {
+			display: none;
+		}
 	}
 </style>
