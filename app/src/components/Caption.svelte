545c5a78d3d04119aa4a9a6019786d77e6de04fd
tinykite <dakota@tinykitelab.com>
2025-03-03 17:13:07 -0600

    Revise article spacing system

--- app/src/components/Caption.svelte
+++ app/src/components/Caption.svelte
@@ -3,13 +3,4 @@
 	let { children } = $props();
 </script>
 
-<p class="article__text article__text--caption">{@render children?.()}</p>
-
-<style>
-	.article__text--caption {
-		font-size: 1rem;
-		font-style: italic;
-		text-align: center;
-		margin: 0;
-	}
-</style>
+<p class="article__caption">{@render children?.()}</p>
