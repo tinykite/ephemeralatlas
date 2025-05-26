34c3edfd12f937d5518b619c39ef36b9d0d80208
tinykite <dakota@tinykitelab.com>
2025-05-26 07:34:57 -0500

    chore: Replace desk with structure tool in Sanity studio

--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,7 +1,7 @@
 import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {structureTool} from 'sanity/structure'
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +10,7 @@ export default defineConfig({
   projectId: '8p4r3cva',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  plugins: [structureTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
